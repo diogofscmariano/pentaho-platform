2359efb80145af97ce99e5e5751eebb8e56717f0
Aliaksandr Pratkin <aliaksandr_pratkin@epam.com>
2015-04-01 12:51:54 +0300

    [BACKLOG-2452] - Add PentahoWadlGeneratorConfig to allow runtime wadl extension based on the build time generated files

--- extensions/src/org/pentaho/platform/web/servlet/PentahoWadlGeneratorConfig.java
+++ extensions/src/org/pentaho/platform/web/servlet/PentahoWadlGeneratorConfig.java
@@ -1,11 +1,5 @@
 package org.pentaho.platform.web.servlet;
 
-import com.sun.jersey.api.wadl.config.WadlGeneratorConfig;
-import com.sun.jersey.api.wadl.config.WadlGeneratorDescription;
-import com.sun.jersey.server.wadl.generators.resourcedoc.WadlGeneratorResourceDocSupport;
-import org.pentaho.platform.api.engine.IPluginManager;
-import org.pentaho.platform.engine.core.system.PentahoSystem;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -14,24 +8,39 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.pentaho.platform.api.engine.IPluginManager;
+import org.pentaho.platform.engine.core.system.PentahoSystem;
+
+import com.sun.jersey.api.wadl.config.WadlGeneratorConfig;
+import com.sun.jersey.api.wadl.config.WadlGeneratorDescription;
+import com.sun.jersey.server.wadl.generators.resourcedoc.WadlGeneratorResourceDocSupport;
+
 public class PentahoWadlGeneratorConfig extends WadlGeneratorConfig {
-  
-  private String getOriginalRequest(){
+
+  private static final String WADL_RESOURCE_FILE = "wadlExtension.xml";
+
+  private String getOriginalRequest() {
     JAXRSPluginServlet jaxrsPluginServlet = getJAXRSPluginServlet();
     String originalRequest = "";
     if ( jaxrsPluginServlet != null ) {
       originalRequest = (String) jaxrsPluginServlet.requestThread.get();
     }
     if ( originalRequest == null || originalRequest.isEmpty() ) {
-      return "/api/application.wadl"; //global api isn't filled
+      return "/api/application.wadl"; // global api isn't filled
     }
     return originalRequest;
   }
-  
-  private WadlGeneratorConfigDescriptionBuilder getBuilderForPluginApi(String plugin){
+
+  private WadlGeneratorConfigDescriptionBuilder getBuilder( String plugin ) {
     String systemPath = PentahoSystem.getApplicationContext().getSolutionPath( "system" );
-    File file = new File( systemPath, plugin + "/resources/wadlExtension.xml" );
-    
+
+    File file;
+    if ( plugin != null ) {
+      file = new File( systemPath, plugin + "/resources/" + WADL_RESOURCE_FILE );
+    } else {
+      file = new File( systemPath, WADL_RESOURCE_FILE );
+    }
+
     try {
       FileInputStream fileInputStream = new FileInputStream( file );
       return generator( WadlGeneratorResourceDocSupport.class ).prop( "resourceDocStream", fileInputStream );
@@ -40,44 +49,34 @@ public class PentahoWadlGeneratorConfig extends WadlGeneratorConfig {
     }
     return null;
   }
-  
-  private WadlGeneratorConfigDescriptionBuilder getBuilderForGlobalApi(){
-    WadlGeneratorConfigDescriptionBuilder builder = generator( WadlGeneratorResourceDocSupport.class );
-    builder.prop( "resourceDocStream", "wadlExtension.xml" );
-    return builder;
-  }
-  
+
   @Override
   public List<WadlGeneratorDescription> configure() {
     String originalRequest = getOriginalRequest();
-    
+
     Pattern pluginPattern = Pattern.compile( ".*\\/plugin\\/([^/]+)\\/api\\/application.wadl" );
-    Pattern globalPattern = Pattern.compile( "\\/api\\/application.wadl" );
     Matcher pluginMatcher = pluginPattern.matcher( originalRequest );
-    Matcher globalMatcher = globalPattern.matcher( originalRequest );
-    
-    WadlGeneratorConfigDescriptionBuilder builder = null;
-    
-    if ( pluginMatcher.matches() ) { //we have a plugin api request
-      String plugin = pluginMatcher.group( 1 );
-      builder = getBuilderForPluginApi(plugin);
-    } else if ( globalMatcher.matches() ) { //we have the global api request
-      builder = getBuilderForGlobalApi();
+
+    String plugin = null;
+    if ( pluginMatcher.matches() ) {
+      plugin = pluginMatcher.group( 1 );
     }
+
+    WadlGeneratorConfigDescriptionBuilder builder = getBuilder( plugin );
 
     if ( builder != null ) {
       return builder.descriptions();
     } else {
       return new ArrayList<WadlGeneratorDescription>();
     }
-    
+
   }
 
   private JAXRSPluginServlet getJAXRSPluginServlet() {
     IPluginManager pluginManager = PentahoSystem.get( IPluginManager.class );
-    
+
     JAXRSPluginServlet jaxrsPluginServlet;
-    
+
     try {
       jaxrsPluginServlet = (JAXRSPluginServlet) pluginManager.getBean( "api" );
     } catch ( Exception e ) {
