28dc4c448f155e34d1a8d30ab7256fd9125caf26
Diogo Mariano <diogo.mariano@webdetails.pt>
2015-04-10 12:01:08 +0100

    [TEST] - Reviewed tests for PentahoWadlGeneratorConfig

--- extensions/src/org/pentaho/platform/web/servlet/PentahoWadlGeneratorConfig.java
+++ extensions/src/org/pentaho/platform/web/servlet/PentahoWadlGeneratorConfig.java
@@ -32,9 +32,40 @@ import com.sun.jersey.api.wadl.config.WadlGeneratorConfig;
 import com.sun.jersey.api.wadl.config.WadlGeneratorDescription;
 import com.sun.jersey.server.wadl.generators.resourcedoc.WadlGeneratorResourceDocSupport;
 
+/**
+ * The wadl configurator class that extends in run time the wadl pointing to a file that adds pre-computed information
+ */
 public class PentahoWadlGeneratorConfig extends WadlGeneratorConfig {
 
-  private String getOriginalRequest() {
+  @Override
+  public List<WadlGeneratorDescription> configure() {
+    String originalRequest = getOriginalRequest();
+
+    Pattern pluginPattern = Pattern.compile( ".*\\/plugin\\/([^/]+)\\/api\\/application.wadl" );
+    Matcher pluginMatcher = pluginPattern.matcher( originalRequest );
+
+    String plugin = null;
+    if ( pluginMatcher.matches() ) {
+      plugin = pluginMatcher.group( 1 );
+    }
+
+    WadlGeneratorConfigDescriptionBuilder builder = getBuilder( plugin );
+
+    if ( builder != null ) {
+      return builder.descriptions();
+    } else {
+      return new ArrayList<WadlGeneratorDescription>();
+    }
+  }
+
+  /**
+   * Gets the original request used to obtain the wadl.
+   * The wadl request is for plugins is changed so we need to retrieve its original form to be able to get the plugin
+   * and point to the right wadl extension file.
+   *
+   * @return String with the original url request
+   */
+  protected String getOriginalRequest() {
     JAXRSPluginServlet jaxrsPluginServlet = getJAXRSPluginServlet();
     String originalRequest = "";
     if ( jaxrsPluginServlet != null ) {
@@ -46,8 +77,15 @@ public class PentahoWadlGeneratorConfig extends WadlGeneratorConfig {
     return originalRequest;
   }
 
-  private WadlGeneratorConfigDescriptionBuilder getBuilder( String plugin ) {
-    List<IWadlDocumentResource> resourceReferences = PentahoSystem.getAll( IWadlDocumentResource.class );
+  /**
+   * Gets the WadlGeneratorConfigDescriptionBuilder that is used to extend the wadl.
+   * It was a property pointing to the InputStream of the file
+   *
+   * @param plugin String with the plugin that is requesting the wadl extension
+   * @return WadlGeneratorConfigDescriptionBuilder that is used to extend the wadl.
+   */
+  protected WadlGeneratorConfigDescriptionBuilder getBuilder( String plugin ) {
+    List<IWadlDocumentResource> resourceReferences = getWadlDocumentResources();
 
     InputStream is = null;
     try {
@@ -71,31 +109,13 @@ public class PentahoWadlGeneratorConfig extends WadlGeneratorConfig {
     return null;
   }
 
-  @Override
-  public List<WadlGeneratorDescription> configure() {
-    String originalRequest = getOriginalRequest();
-
-    Pattern pluginPattern = Pattern.compile( ".*\\/plugin\\/([^/]+)\\/api\\/application.wadl" );
-    Matcher pluginMatcher = pluginPattern.matcher( originalRequest );
-
-    String plugin = null;
-    if ( pluginMatcher.matches() ) {
-      plugin = pluginMatcher.group( 1 );
-    }
-
-    WadlGeneratorConfigDescriptionBuilder builder = getBuilder( plugin );
-
-    if ( builder != null ) {
-      return builder.descriptions();
-    } else {
-      return new ArrayList<WadlGeneratorDescription>();
-    }
-
-  }
-
-  private JAXRSPluginServlet getJAXRSPluginServlet() {
+  /**
+   * Gets the getJAXRSPluginServlet for api that stores the the original request for the wadl
+   *
+   * @return JAXRSPluginServlet with the stored original request
+   */
+  protected JAXRSPluginServlet getJAXRSPluginServlet() {
     IPluginManager pluginManager = PentahoSystem.get( IPluginManager.class );
-
     JAXRSPluginServlet jaxrsPluginServlet;
 
     try {
@@ -104,5 +124,14 @@ public class PentahoWadlGeneratorConfig extends WadlGeneratorConfig {
       return null;
     }
     return jaxrsPluginServlet;
+  }
+
+  /**
+   * Returns the list of the wadl extension declared beans
+   *
+   * @return List<IWadlDocumentResource> with the declared beans
+   */
+  protected List<IWadlDocumentResource> getWadlDocumentResources() {
+    return PentahoSystem.getAll( IWadlDocumentResource.class );
   }
 }
--- extensions/test-src/org/pentaho/platform/web/servlet/PentahoWadlGeneratorConfigTest.java
+++ extensions/test-src/org/pentaho/platform/web/servlet/PentahoWadlGeneratorConfigTest.java
@@ -17,18 +17,24 @@
 
 package org.pentaho.platform.web.servlet;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.jersey.api.wadl.config.WadlGeneratorConfig;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.pentaho.platform.api.engine.IApplicationContext;
 import org.pentaho.platform.api.engine.IPluginManager;
+import org.pentaho.platform.api.util.IWadlDocumentResource;
 import org.pentaho.platform.engine.core.system.PentahoSystem;
 
 import com.sun.jersey.api.wadl.config.WadlGeneratorDescription;
@@ -42,39 +48,92 @@ public class PentahoWadlGeneratorConfigTest {
     spyConfig = spy( new PentahoWadlGeneratorConfig() );
   }
 
-  private void initApplicationContext( String postfixPath ) throws Exception {
-    IApplicationContext applicationContext = mock( IApplicationContext.class );
-    String path = new java.io.File( "." ).getCanonicalPath();
-    when( applicationContext.getSolutionPath( anyString() ) ).thenReturn( path + postfixPath );
-    PentahoSystem.setApplicationContext( applicationContext );
+  @Test
+  public void testConfigure() throws Exception {
+    List<WadlGeneratorDescription> result;
+
+    doReturn( null ).when( spyConfig ).getBuilder( null );
+
+    String originalRequest = "/api/application.wadl";
+    doReturn( originalRequest ).when( spyConfig ).getOriginalRequest();
+    result = spyConfig.configure();
+    verify( spyConfig, times( 1 ) ).getBuilder( null );
+    verify( spyConfig, times( 1 ) ).getOriginalRequest();
+    assertTrue( result.size() == 0 );
+
+    originalRequest = "/asdffsadasdfdsaf/adsafds.wadl";
+    doReturn( originalRequest ).when( spyConfig ).getOriginalRequest();
+    result = spyConfig.configure();
+    verify( spyConfig, times( 2 ) ).getBuilder( null );
+    verify( spyConfig, times( 2 ) ).getOriginalRequest();
+    assertTrue( result.size() == 0 );
+
+    originalRequest = "/plugin/reporting/api/application.wadl";
+    doReturn( originalRequest ).when( spyConfig ).getOriginalRequest();
+    result = spyConfig.configure();
+    verify( spyConfig, times( 1 ) ).getBuilder( "reporting" );
+    verify( spyConfig, times( 3 ) ).getOriginalRequest();
+    assertTrue( result.size() == 0 );
+
+    List<WadlGeneratorConfig> wadlGeneratorConfigList = new ArrayList<WadlGeneratorConfig>();
+    wadlGeneratorConfigList.add( mock( WadlGeneratorConfig.class ) );
+
+    WadlGeneratorConfig.WadlGeneratorConfigDescriptionBuilder builder = mock(
+        WadlGeneratorConfig.WadlGeneratorConfigDescriptionBuilder.class );
+    doReturn( builder ).when( spyConfig ).getBuilder( null );
+    doReturn( builder ).when( spyConfig ).getBuilder( "reporting" );
+    doReturn( wadlGeneratorConfigList ).when( builder ).descriptions();
+
+    originalRequest = "/api/application.wadl";
+    doReturn( originalRequest ).when( spyConfig ).getOriginalRequest();
+    result = spyConfig.configure();
+    verify( spyConfig, times( 3 ) ).getBuilder( null );
+    verify( spyConfig, times( 4 ) ).getOriginalRequest();
+    assertTrue( result.size() == 1 );
+
+    originalRequest = "/asdffsadasdfdsaf/adsafds.wadl";
+    doReturn( originalRequest ).when( spyConfig ).getOriginalRequest();
+    result = spyConfig.configure();
+    verify( spyConfig, times( 4 ) ).getBuilder( null );
+    verify( spyConfig, times( 5 ) ).getOriginalRequest();
+    assertTrue( result.size() == 1 );
+
+    originalRequest = "/plugin/reporting/api/application.wadl";
+    doReturn( originalRequest ).when( spyConfig ).getOriginalRequest();
+    result = spyConfig.configure();
+    verify( spyConfig, times( 2 ) ).getBuilder( "reporting" );
+    verify( spyConfig, times( 6 ) ).getOriginalRequest();
+    assertTrue( result.size() == 1 );
   }
 
   @Test
-  public void testNoWadlFile() throws Exception {
-    initApplicationContext( "\\test-src-wrongpath" );
-    List<WadlGeneratorDescription> result = spyConfig.configure();
-    Assert.assertTrue( result.size() == 0 );
+  public void testGetBuilder() throws Exception {
+    WadlGeneratorConfig.WadlGeneratorConfigDescriptionBuilder result;
+
+    List<IWadlDocumentResource> list = new ArrayList<IWadlDocumentResource>();
+    IWadlDocumentResource documentResource1 = mock( IWadlDocumentResource.class ),
+        documentResource2 = mock( IWadlDocumentResource.class ),
+        documentResource3 = mock( IWadlDocumentResource.class );
+    list.add( documentResource1 );
+    list.add( documentResource2 );
+    list.add( documentResource3 );
+
+    doReturn( false ).when( documentResource1 ).isFromPlugin();
+    doReturn( true ).when( documentResource2 ).isFromPlugin();
+    doReturn( true ).when( documentResource3 ).isFromPlugin();
+    doReturn( "" ).when( documentResource1 ).getPluginId();
+    doReturn( "reporting" ).when( documentResource2 ).getPluginId();
+    doReturn( "data-access" ).when( documentResource3 ).getPluginId();
+    doReturn( mock( InputStream.class ) ).when( documentResource1 ).getResourceAsStream();
+    doReturn( mock( InputStream.class ) ).when( documentResource2 ).getResourceAsStream();
+    doThrow( new IOException() ).when( documentResource3 ).getResourceAsStream();
+    doReturn( list ).when( spyConfig ).getWadlDocumentResources();
+
+    assertNotNull( spyConfig.getBuilder( null ) );
+    assertNotNull( spyConfig.getBuilder( "reporting" ) );
+    assertNull( spyConfig.getBuilder( "data-access" ) );
+
+    doThrow( new IOException() ).when( documentResource1 ).getResourceAsStream();
+    assertNull( spyConfig.getBuilder( null ) );
   }
-
-  @Test
-  public void testWadlExist() throws Exception {
-    initApplicationContext( "\\test-src" );
-    List<WadlGeneratorDescription> result = spyConfig.configure();
-    Assert.assertTrue( result.size() == 1 );
-  }
-
-  @Test
-  public void testWadlExistPlugin() throws Exception {
-    // init plugin
-    JAXRSPluginServlet pluginServlet = mock( JAXRSPluginServlet.class );
-    pluginServlet.requestThread.set( "http://localhost:8080/pentaho/plugin/data-access/api/application.wadl" );
-    IPluginManager pluginManager = mock( IPluginManager.class );
-    when( pluginManager.getBean( anyString() ) ).thenReturn( pluginServlet );
-    PentahoSystem.registerObject( pluginManager );
-
-    initApplicationContext( "\\test-src" );
-    List<WadlGeneratorDescription> result = spyConfig.configure();
-    Assert.assertTrue( result.size() == 1 );
-  }
-
 }
